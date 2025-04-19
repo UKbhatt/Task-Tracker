53c28f3ae12573283f75a1a963668baf94c26c2a
Utkarsh Bhatt <ubhatt2004@gmail.com>
2025-04-19 16:09:51 +0530

    Update: To check user's login status

--- lib/Services/supabase_services.dart
+++ lib/Services/supabase_services.dart
@@ -4,10 +4,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class SupabaseServices {
   static final client = Supabase.instance.client;
 
-  static void init() async{
+  static void init() async {
     await Supabase.initialize(
-      url: dotenv.env['SUPABASE_URL'] ?? 'default_supabase_url',
-      anonKey: dotenv.env['ANON_KEY'] ?? 'default_ANON_url'
-      );
+        url: dotenv.env['SUPABASE_URL'] ?? 'default_supabase_url',
+        anonKey: dotenv.env['ANON_KEY'] ?? 'default_ANON_url');
   }
+
+  static bool get isLoggedIn => client.auth.currentUser != null;
 }
